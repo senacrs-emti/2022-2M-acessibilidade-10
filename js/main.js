4fde00298a5e425b07f2bdf7066551bec451bb4c
Fah <80393387+Hollow-Soul@users.noreply.github.com>
2022-04-28 12:25:04 -0300

    update 0.3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-function setTheme(theme) {
-        main.css.root.style.setProperty('--blue', 'rgb(0, 255, 0)');
-    }
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (elemente, style) => window.getComputedStyle(html).getPropertyValue("--bg")
+
+const initialColors = {
+    blue: getStyle(html, "--blue"),
+    darkBlue: getStyle(html, "--darkBlue"),
+    white: getStyle(html, "--white"),
+}
+
+const greyScale = {
+    blue: "#",
+    darkBlue: "#",
+}
+
+const transformKey = key => 
+"--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+const changeColors = (colors) =>{
+    Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+checkbox.addEventListener("change", ({target}) =>{
+    target.checked ? changeColors(greyScale) : changeColors(initialColors)
+})
